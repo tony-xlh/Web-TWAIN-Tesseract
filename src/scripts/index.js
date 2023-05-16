778281d666375b7aea93cde627fddac635066363
xulihang <xulihanghai@163.com>
2023-05-16 10:11:41 +0800

    load DWT

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,23 @@
 import '../styles/index.scss';
+import Dynamsoft from "dwt";
 
 console.log('webpack starterkit');
+window.onload = function(){
+  initDWT();
+};
+
+function initDWT(){
+  const containerID = "dwtcontrolcontainer";
+  Dynamsoft.DWT.RegisterEvent('OnWebTwainReady', () => {
+    const DWObject = Dynamsoft.DWT.GetWebTwain(containerID);
+    DWObject.Viewer.width = "100%";
+    DWObject.Viewer.height = "100%";
+  });
+  
+  Dynamsoft.DWT.ResourcesPath = "/dwt-resources";
+  Dynamsoft.DWT.Containers = [{
+      WebTwainId: 'dwtObject',
+      ContainerId: containerID
+  }];
+  Dynamsoft.DWT.Load();
+}
